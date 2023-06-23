d01be8d7fca77c425639f58133db763e89e2756e
luclucyt <88875@glr.nl>
2023-06-23 08:48:01 +0200

    commit

--- home.php
+++ home.php
@@ -2,12 +2,14 @@
 <html>
 
 <head>
-    <title>Vote for Fan Favorite</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="navbar.css">
+    
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js" defer></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js" defer></script>
     <script src="login.js" defer></script>
+    
+    <title>Vote for Fan Favorite</title>
 </head>
 
 <header>
